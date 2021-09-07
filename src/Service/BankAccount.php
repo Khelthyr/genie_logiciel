a910ea534fb55c410a218f484937dbfc732fbecb
Khelthyr <cedric.nils.vincendeau@gmail.com>
2021-09-07 15:28:30 +0200

    add substract to account + test

--- src/Service/BankAccount.php
+++ src/Service/BankAccount.php
@@ -4,15 +4,25 @@ final class BankAccount
 {
 	protected int $balance = 0;
 
-	public function __construct()
+	public function __construct(int $balance = 0)
 	{
-		$this->balance = 0;
+		$this->balance = $balance;
 	}
 
 	public function credit(int $balance): BankAccount
 	{
 		$this->balance = (int) $balance;
 		return $this;
+	}
+
+	public function debiter(int $balance): BankAccount
+	{
+		if ($this->balance < $balance) {
+			throw new BankAccountException('Insufficient funds');
+		}else{
+			$this->balance -= (int) $balance;
+			return $this;
+		}
 	}
 
 	public function getBalance(): int
--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 require "C:\Users\cedri\Desktop\M2 EXO\genie_logiciel\src\Service\BankAccount.php";
+require "C:\Users\cedri\Desktop\M2 EXO\genie_logiciel\src\Service\BankAccountException.php";
 
 
 final class BankAccountTest extends TestCase
@@ -19,5 +20,18 @@ final class BankAccountTest extends TestCase
         $this->assertEquals($account->getBalance(), 100);
     }
 
-    public function test
+    public function testBalancePostDebiter(): void
+    {
+        $account = new BankAccount(100);
+        $account->debiter(50);
+        $this->assertEquals($account->getBalance(), 50);
+    }
+
+    public function testBalancePostDebiterException(): void
+    {
+        $account = new BankAccount();
+        $this->expectException(BankAccountException::class);
+        $account->debiter(50);
+        
+    }
 }
